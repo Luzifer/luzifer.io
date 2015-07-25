76b72def7c124b187353e998927c00f381518579
Knut Ahlers <knut@ahlers.me>
2015-07-26 01:59:12 +0200

    Generate also 1000x500 for post headers

--- static/projects/webshot.sh
+++ static/projects/webshot.sh
@@ -4,7 +4,8 @@ URL=$1
 TARGET=$2
 
 phantomjs webshot.js ${URL}
-convert -crop 1920x1440+0+0 -resize 400x300 webshot.png ${TARGET}_400x300.png
-convert -crop 1920x1280+0+0 -resize 645x430 webshot.png ${TARGET}_645x430.png
-convert -crop 1920x1080+0+0 webshot.png ${TARGET}_1920x1080.png
+convert -resize 400x400 -crop 400x300+0+0 webshot.png ${TARGET}_400x300.png
+convert -resize 645x645 -crop 645x430+0+0 webshot.png ${TARGET}_645x430.png
+convert -resize 1000x1000 -crop 1000x500+0+0 webshot.png ${TARGET}_1000x500.png
+convert -resize 1920x1920 -crop 1920x1080+0+0 webshot.png ${TARGET}_1920x1080.png
 rm webshot.png
